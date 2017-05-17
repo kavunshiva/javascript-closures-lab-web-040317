ef0a457ef05d5addf89c3c91ee75b6299cefc82e
kavunshiva <kavunshiva@gmail.com>
2017-05-17 15:47:46 -0400

    sharks with frickin laser beams

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much."
+
+const bumpCounter = () => {
+  let counter = 0
+  let addBump = () => counter++
+  let getBumps = () => (counter)
+  return {addBump, getBumps}
+}
+
+const createAnimal = (animalType) => {
+  return (deadlyDevice) => ({animalType, deadlyDevice})
+}
+
+const sharkCreator = createAnimal("Shark")
+
+const sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+const sharkWithFrickinCannon = sharkCreator("Cannon")
